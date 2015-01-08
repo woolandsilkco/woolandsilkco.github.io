097a690bac7a02fcfc819e0232d166462d9544a9
Frazer Seymour <pseymour@uoguelph.ca>
2015-01-08 14:47:20 -0500

    Testing loading classes dynamically.

---
+++ js/classes.js
@@ -0,0 +1,23 @@
+function doStuffWithClasses(data)
+{
+    console.log(data);
+}
+
+function loadClasses(onLoad)
+{
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://api.github.com/repos/woolandsilkco/woolandsilkco.github.io/classes.json", true);
+    request.responseType = "arraybuffer";
+
+    request.onload = function()
+    {
+        onLoad(request.response);
+    }
+
+    request.onerror = function()
+    {
+        alert('BufferLoader: XHR error');
+    }
+
+    request.send();
+}
