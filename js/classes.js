0b7be1adff4f5fda8f2663fabc4f989afbea124f
Frazer Seymour <pseymour@uoguelph.ca>
2015-01-08 14:55:51 -0500

    Using default responseType.

--- js/classes.js
+++ js/classes.js
@@ -7,7 +7,6 @@ function loadClasses(onLoad)
 {
     var request = new XMLHttpRequest();
     request.open("GET", "https://api.github.com/repos/woolandsilkco/woolandsilkco.github.io/contents/classes.json", true);
-    request.responseType = "arraybuffer";
 
     request.onload = function()
     {
