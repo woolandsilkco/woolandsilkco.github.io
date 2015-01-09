1393ff72f8c0f19159d99ef0fabcc178e24eaf0c
Frazer Seymour <frazer@seymourandco.com>
2015-01-09 01:14:17 -0500

    Rough draft of class preview code.

--- js/classes.js
+++ js/classes.js
@@ -1,6 +1,39 @@
 function doStuffWithClasses(data)
 {
-    console.log(data);
+    var today = new Date();
+    var class1 = null;
+    var class2 = null;
+
+    for (var i = 0; i < data.length; i++)
+    {
+        for (var j = 0; j < data[i].dates.length; j++)
+        {
+            var date2 = new Date(data[i].dates[j]);
+            
+            var timeDiff = Math.abs(date2.getTime() - today.getTime());
+
+            if (!class1)
+            {
+                class1 = [data[i], timeDiff];
+            }
+            else if (!class2)
+            {
+                class2 = [data[i], timeDiff];
+            }
+            else if (timeDiff < class1[1])
+            {
+                class2 = class1;
+                class1 = [data[i], timeDiff];
+            }
+            else if (timeDiff < class2[1])
+            {
+                class2 = [data[i], timeDiff];
+            }
+        }
+    }
+
+    console.log(class1);
+    console.log(class2);
 }
 
 function loadClasses(onLoad)
