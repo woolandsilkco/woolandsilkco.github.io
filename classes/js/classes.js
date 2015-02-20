6733cc6cee3b27130cd8e3807edb8fdb60bbc7c6
Frazer Seymour <frazer@seymourandco.com>
2015-02-19 20:31:38 -0500

    Alternate debugging.

--- classes/js/classes.js
+++ classes/js/classes.js
@@ -24,7 +24,7 @@ function displayClassesPreview(data)
     else
     {
         var obj = { "array" : classes };
-        console.log(obj);
+        alert(obj);
         child.innerHTML = doTClasses( obj );
         child = child.firstChild;
         document.getElementById('insertClasses').appendChild(child); 
